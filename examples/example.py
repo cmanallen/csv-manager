ab04a103551bf30ec6ee2d7441fb90182ecb79ac
cmanallen <cmanallen90@gmail.com>
2014-05-03 08:38:32 -0400

    Removed comment code in example.py

--- examples/example.py
+++ examples/example.py
@@ -1,8 +1,6 @@
 from csv_manager.parser import CsvToObject
 
 dataset = CsvToObject('example.csv', True).parse()
-# for key, value in dataset.items():
-	# print(value)
 
 for key, item in dataset.items():
 	print(item)
